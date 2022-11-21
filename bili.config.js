dd048a40cf17649a7339dea00713ccf318c623ba
Pedro Asseiro <pedro.asseiro@twa.pt>
2022-11-21 14:47:32 +0000

    Novos configs

--- bili.config.js
+++
@@ -1,9 +0,0 @@
-const vue = require('rollup-plugin-vue')
-
-module.exports = {
-  // banner: true,
-  format: ['umd-min', 'cjs-min'],
-  // css: true,
-  plugins: [vue({ css: true })],
-  outDir: 'lib',
-}
